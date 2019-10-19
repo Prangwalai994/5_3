f161106d653367db137e22ad7f8e79fe2e77304b
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-20 01:42:41 +0700

    Add project files.

---
+++ Refactioning_Towards5_3/Mocking/FileReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refactioning_Towards5_3.Mocking
+{
+    public class FileReader
+    {
+        public string Read(string path)
+        {
+             return File.ReadAllText(path);
+        }
+    }
+}
